a25d07ba9ceb35e4cab3b47b0cc40d6ee2c60f0f
MakdiManush <aryangoel121@gmail.com>
2024-02-02 14:41:27 -0500

    Tic Tac Toe
    Created Tic Tac Toe from scratch, put all the logic behind it with javaScript, styled it and it works fine. Covered all the corner cases.

--- Tic Tac Toe HTML CSS JS/script.js
+++ Tic Tac Toe HTML CSS JS/script.js
@@ -1,75 +1,90 @@
+// Initializing all the major nodes
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector(".reset");
-let msg = document.querySelector(".msg");
 let turnO = true;
+console.log("Turn of O");
+let resetBtn = document.querySelector(".reset");
+let newGame = document.querySelector(".newGame");
+let msgContainer = document.querySelector(".msgContainer");
 
-const winPatterns = [
+// Winning Patterns
+let winPatterns = [
     [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
     [0, 3, 6],
-    [0, 4, 8],
     [1, 4, 7],
     [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8]
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
-console.log("Turn of Player = O");
+// Disable all the boxes
+let disableBoxes = () =>{
+    boxes.forEach((box) => {
+        box.disabled = true;
+    })
+}
+
+// Enable all the boxes
+let enableBoxes = () => {
+    boxes.forEach((box)=>{
+        box.innerText = "";
+        box.disabled = false;
+    })
+}
+
+// Main Logic for Input
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if(turnO){
             box.innerText = "O";
             turnO = false;
-            console.log("Turn of Player = X");
+            console.log("Turn of X");
         }
         else{
             box.innerText = "X";
             turnO = true;
-            console.log("Turn of Player = O");
+            console.log("Turn of O");
         }
         box.disabled = true;
-
         checkWinner();
     })
 })
 
-const checkWinner = () =>{
-    for(let pattern of winPatterns){        
-        let pos1Val = boxes[pattern[0]].innerText
-        let pos2Val = boxes[pattern[1]].innerText
-        let pos3Val = boxes[pattern[2]].innerText
+// Checking the Winner according to the winning patterns
+let checkWinner = () => {
+    for(let pattern of winPatterns){
+        pos1Val = boxes[pattern[0]].innerText;
+        pos2Val = boxes[pattern[1]].innerText;
+        pos3Val = boxes[pattern[2]].innerText;
 
         if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
             if(pos1Val == pos2Val && pos1Val == pos3Val){
-                for(let box of boxes){
-                    box.disabled = true;
-                }
-                const winText = "Congratulations! Winner is: " + pos1Val;
-                printWinner(winText);
+                disableBoxes();
+                let winText = pos1Val + " is the Winner!";
+                displayWinner(winText);
+                console.log("Winner " + winText);
             }
         }
+        
     }
 }
 
-const msgContainer = document.querySelector(".msgContainer");
-const printWinner = (winText) =>{
-    msg.innerText = winText;
-    msgContainer.style.visibility = "visible";
+// Display Winner
+let displayWinner = (winText) => {
+    document.querySelector(".winMsg").innerText = winText;
+    msgContainer.classList.remove("hide");
 }
 
-resetBtn.addEventListener("click", () => {
-    boxes.forEach((box)=>{
-        box.innerText="";
-        box.disabled = false;
-    })
-});
 
-let newGame = document.querySelector(".newGame");
-newGame.addEventListener("click", () => {
-    boxes.forEach((box)=>{
-        box.innerText="";
-        box.disabled = false;
-    })
-    msg.innerText = "";
-    msgContainer.style.visibility = "hidden";
+// Reset the game
+resetBtn.addEventListener("click", () => {
+    enableBoxes();
+})
+
+
+// Start a New Game
+newGame.addEventListener("click", ()=>{
+    enableBoxes();
+    msgContainer.classList.add("hide");
 })
